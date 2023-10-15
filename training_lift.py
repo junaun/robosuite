e6787df0ee980bee07ae4d8d5be31fdbc65af983
junaun <jtan515@e.ntu.edu.sg>
2023-10-15 17:29:50 +0800

    vectorized env

--- training_lift.py
+++ training_lift.py
@@ -1,12 +1,7 @@
 import argparse
 import robosuite as suite
 from robosuite.wrappers.gym_wrapper import GymWrapper
-import gymnasium as gym
-from stable_baselines3.common.vec_env import SubprocVecEnv
-from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.monitor import Monitor
-import time
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
 from stable_baselines3.common.results_plotter import load_results, ts2xy
 from stable_baselines3.common.callbacks import BaseCallback
 import numpy as np
@@ -94,42 +89,42 @@ else:
                         self.model.save(self.save_path)
             return True
 
-    env = GymWrapper(suite.make(
-        env_name="Lift", # try with other tasks like "Stack" and "Door"
-        robots="UR5e",  # try with other robots like "Sawyer" and "Jaco"
-        has_renderer=True,
-        has_offscreen_renderer=False,
-#        controller_configs = controller_config,
-        use_object_obs=False,                   # don't provide object observations to agent
-        use_camera_obs=False,
-        reward_shaping=True,                    # use a dense reward signal for learning
-        reward_scale=1.0,
-        horizon = 500,
-        control_freq=20,                        # control should happen fast enough so that simulation looks smooth
-        ignore_done=False,
-        hard_reset=False,
-    ))
-    env = Monitor(env, filename)
+    def make_env(i):
+        env = GymWrapper(suite.make(
+            env_name="Lift", # try with other tasks like "Stack" and "Door"
+            robots="UR5e",  # try with other robots like "Sawyer" and "Jaco"
+            has_renderer=True,
+            has_offscreen_renderer=False,
+            use_object_obs=False,                   # don't provide object observations to agent
+            use_camera_obs=False,
+            reward_shaping=True,                    # use a dense reward signal for learning
+            reward_scale=1.0,
+            horizon = 500,
+            control_freq=20,                        # control should happen fast enough so that simulation looks smooth
+            ignore_done=False,
+            hard_reset=False,
+        ))
+        # env = Monitor(env, f'test_{i}.csv')
+        return env
+
+if __name__ == '__main__':
+    num_envs = 16
+    envs = [lambda i=i: make_env(i) for i in range(num_envs)]
+    env = SubprocVecEnv(envs)
+    env = VecMonitor(env, filename=filename)
+    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=filename)
+
     policy_kwargs = dict(
         net_arch=[256, 256]
     )
 
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=filename)
-
-    if os.path.exists(f'{filename}/best_model.zip'):
-        # Do something if the folder exists
-        print(f"The folder '{filename}' exists.")
-        model = PPO.load(f'{filename}/best_model.zip', env=env)
-        model.learn(total_timesteps=25e4, progress_bar=True, log_interval=20, callback=callback)
-    else:
-        # Do something else if the folder doesn't exist
-        print(f"The folder '{filename}' does not exist.")
-        model = PPO("MultiInputPolicy", env, verbose=1, batch_size=256, policy_kwargs=policy_kwargs)
-        print('start learning')
-        model.learn(total_timesteps=5e5, progress_bar=True, log_interval=10, callback=callback)
-        del model
-        # model.save(f'{filename}/best_model')
-        for i in range(10):
-            model = PPO.load(f'{filename}/best_model.zip', env=env)
-            model.learn(total_timesteps=5e5, progress_bar=True, log_interval=20, callback=callback)
+    print(f"The folder '{filename}' does not exist.")
+    model = PPO("MultiInputPolicy", env, verbose=1, batch_size=256, policy_kwargs=policy_kwargs)
+    print('start learning')
+    model.learn(total_timesteps=5e6, progress_bar=True, log_interval=10, callback=callback)
+    # del model
+    # # model.save(f'{filename}/best_model')
+    # for i in range(10):
+    #     model = PPO.load(f'{filename}/best_model.zip', env=env)
+    #     model.learn(total_timesteps=5e5, progress_bar=True, log_interval=20, callback=callback)
 
